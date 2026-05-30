0679b65b578cd25464726621d26bbd545783c918
DueGin <1917764907@qq.com>
2026-05-30 19:26:02 +0800

    feat: add channel monitor schema and migrations

--- backend/ent/ent.go
+++ backend/ent/ent.go
@@ -17,6 +17,10 @@ import (
 	"github.com/Wei-Shaw/sub2api/ent/announcement"
 	"github.com/Wei-Shaw/sub2api/ent/announcementread"
 	"github.com/Wei-Shaw/sub2api/ent/apikey"
+	"github.com/Wei-Shaw/sub2api/ent/channelmonitor"
+	"github.com/Wei-Shaw/sub2api/ent/channelmonitordailyrollup"
+	"github.com/Wei-Shaw/sub2api/ent/channelmonitorhistory"
+	"github.com/Wei-Shaw/sub2api/ent/channelmonitorrequesttemplate"
 	"github.com/Wei-Shaw/sub2api/ent/errorpassthroughrule"
 	"github.com/Wei-Shaw/sub2api/ent/giftbalancerecord"
 	"github.com/Wei-Shaw/sub2api/ent/group"
@@ -115,6 +119,10 @@ func checkColumn(t, c string) error {
 			accountgroup.Table:                   accountgroup.ValidColumn,
 			announcement.Table:                   announcement.ValidColumn,
 			announcementread.Table:               announcementread.ValidColumn,
+			channelmonitor.Table:                 channelmonitor.ValidColumn,
+			channelmonitordailyrollup.Table:      channelmonitordailyrollup.ValidColumn,
+			channelmonitorhistory.Table:          channelmonitorhistory.ValidColumn,
+			channelmonitorrequesttemplate.Table:  channelmonitorrequesttemplate.ValidColumn,
 			errorpassthroughrule.Table:           errorpassthroughrule.ValidColumn,
 			giftbalancerecord.Table:              giftbalancerecord.ValidColumn,
 			group.Table:                          group.ValidColumn,
